3c0192d86f73857977ca8330e68b7abf650894ca
ᦖєgιт <support@legitsoulja.info>
2018-02-17 15:38:45 -0500

    Create SteamSDK.php

---
+++ SteamSDK.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace {
+    class SteamSDK
+    {
+        
+        
+    }
+    class Config
+    {
+        protected const _KEY = 'STEAM_API_KEY';
+        protected const _API = 'http://api.steampowered.com/%s/%s/%s';
+        protected const _FORMAT = 'json';
+        
+        public static function getQuery($a)
+        {
+            $a['key']    = self::_KEY;
+            $a['format'] = self::_FORMAT;
+            return '/?' . http_build_query($a);
+        }
+        
+        public static function getURL($interface, $method, $version, $data)
+        {
+            $url = sprintf(self::_API, $interface, $method, $version, self::_KEY);
+            $url .= self::getQuery($data);
+            return $url;
+        }
+        
+        public static function request($url)
+        {
+            $curl = curl_init($url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            $resp;
+            try {
+                $resp = json_decode(curl_exec($curl), true);
+            }
+            catch (\Exception $ex) {
+                throw new \Exception($ex);
+            }
+            curl_close($curl);
+            return $resp;
+        }
+        
+    }
+}
+
+
+namespace SteamSDK {
+    
+    class ISteamNews
+    {
+        
+        protected const NAME = "ISteamNews";
+        
+        public static function GetNewsForApp($appID, $count = 5, $maxLength = 25)
+        {
+            return \Config::request(\Config::getURL(self::NAME, 'GetNewsForApp', 'v0002', array(
+                'appid' => $appID,
+                'count' => $count,
+                'maxLength' => $maxLength
+            )));
+        }
+        
+        public static function getGlobalAchievementPercentagesForApp($appID)
+        {
+            return \Config::request(\Config::getURL(self::NAME, 'GetGlobalAchievementPercentagesForApp', 'v0002', array(
+                'gameid' => $appID
+            )));
+        }
+        
+        public static function getGlobalStatsForGame($appID, $count = 5)
+        {
+            
+        }
+        
+        public static function getPlayerSummaries($steamid)
+        {
+            return \Config::request(\Config::getURL(self::NAME, 'GetPlayerSummaries', 'v0002', array(
+                'steamids' => $steamid
+            )));
+        }
+        
+    }
+    
+    class ISteamUserStats
+    {
+        
+    }
+    
+    class ISteamUser
+    {
+    }
+    
+    
+}
